d3e19dcf0e8b251bedc2e9041c0d68837c7786ae
OgliariNatan <natanogliari@gmail.com>
2024-12-11 11:05:19 -0300

    adicionando home para os projetos

--- djangotutorial/mysite/views.py
+++ djangotutorial/mysite/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Bem-vindo à página inicial!")
+    return render(request, 'home.html')
+    #return HttpResponse("Bem-vindo à página inicial!")
